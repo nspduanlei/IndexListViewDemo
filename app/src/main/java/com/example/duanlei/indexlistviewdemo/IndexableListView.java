69c4b9de81d0acf9e07141330eaeaa976babff36
duanlei <455483293@qq.com>
2015-12-29 11:02:18 +0800

    索引条的绘制

--- app/src/main/java/com/example/duanlei/indexlistviewdemo/IndexableListView.java
+++ app/src/main/java/com/example/duanlei/indexlistviewdemo/IndexableListView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 woozzu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.duanlei.indexlistviewdemo;
 
 import android.content.Context;
@@ -9,16 +25,16 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 
 /**
- * Created by duanlei on 15/12/23.
+ * 自定义索引列表
+ *
+ * @author by 佚名
+ *
  */
 public class IndexableListView extends ListView {
 
     private boolean mIsFastScrollEnabled = false;
-    //用于绘制索引条
     private IndexScroller mScroller = null;
-
-    private GestureDetector mGestureDetector;
-
+    private GestureDetector mGestureDetector = null;
 
     public IndexableListView(Context context) {
         super(context);
@@ -28,28 +44,25 @@ public class IndexableListView extends ListView {
         super(context, attrs);
     }
 
-    public IndexableListView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public IndexableListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
     }
 
     @Override
     public boolean isFastScrollEnabled() {
-        return super.isFastScrollEnabled();
+        return mIsFastScrollEnabled;
     }
 
     @Override
     public void setFastScrollEnabled(boolean enabled) {
-        //super.setFastScrollEnabled(enabled);
-
         mIsFastScrollEnabled = enabled;
-
         if (mIsFastScrollEnabled) {
-            if (mScroller == null) {
+            if (mScroller == null)
                 mScroller = new IndexScroller(getContext(), this);
-            }
         } else {
             if (mScroller != null) {
-               // mScroller.hide();
+                mScroller.hide();
+                mScroller = null;
             }
         }
     }
@@ -57,50 +70,56 @@ public class IndexableListView extends ListView {
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        if (mScroller != null) {
-            //绘制右侧索引条
+
+        // Overlay index bar
+        if (mScroller != null)
             mScroller.draw(canvas);
-        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-
-        // 如果mScroller自己来处理触摸事件，该方法返回true
-//        if (mScroller != null && mScroller.onTouchEvent(ev)) {
-//            return true;
-//        }
+        // Intercept ListView's touch event
+        if (mScroller != null && mScroller.onTouchEvent(ev))
+            return true;
 
         if (mGestureDetector == null) {
-            //使用手势处理触摸事件
-            mGestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
-                @Override
-                public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                    //显示右侧的索引条
-//                    mScroller.show();
-                    return super.onFling(e1, e2, velocityX, velocityY);
-                }
-            });
-        }
+            // 创建一个GestureDetector（手势探测器）
+            mGestureDetector = new GestureDetector(getContext(),
+                    new GestureDetector.SimpleOnGestureListener() {
 
+                        @Override
+                        public boolean onFling(MotionEvent e1, MotionEvent e2,
+                                               float velocityX, float velocityY) {
+                            // If fling happens, index bar shows
+                            // 显示索引条
+                            mScroller.show();
+                            return super.onFling(e1, e2, velocityX, velocityY);
+                        }
+
+                    });
+        }
         mGestureDetector.onTouchEvent(ev);
+
         return super.onTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return true;
     }
 
     @Override
     public void setAdapter(ListAdapter adapter) {
         super.setAdapter(adapter);
-        if (mScroller != null) {
+        if (mScroller != null)
             mScroller.setAdapter(adapter);
-        }
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        if (mScroller != null) {
+        if (mScroller != null)
             mScroller.onSizeChanged(w, h, oldw, oldh);
-        }
-
     }
+
 }
